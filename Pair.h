5030a7662f3983273f01e66af9aa24427d994eda
NitayNahary <nitay.nahary@campus.technion.ac.il>
2023-05-07 16:43:54 +0300

    transfer to gitHub

---
+++ Pair.h
@@ -0,0 +1,27 @@
+//
+// Created by user on 06/05/2023.
+//
+#ifndef PAIR_H
+#define PAIR_H
+
+template<class T, class K>
+struct Pair{
+private:
+    T m_data;
+    K m_key;
+public:
+    Pair(const T& data,const K& key):  m_data(data), m_key(key){}
+    Pair(const Pair<T,K>& p):m_data(p.m_data), m_key(p.m_key){}
+    Pair& operator= (const Pair& p) = default;
+    ~Pair() = default;
+    T& data(){
+        return m_data;
+    }
+    const T& data() const{
+        return m_data;
+    }
+    const K& key() const{
+        return m_key;
+    }
+};
+#endif //PAIR_H
